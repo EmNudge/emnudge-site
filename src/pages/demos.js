a97c9f7cff854c234cff2fe998b86aa81f440876
emnudge <emnudge@gmail.com>
2019-05-17 11:14:31 -0400

    fixed demo page styling

--- src/pages/demos.js
+++ src/pages/demos.js
@@ -13,24 +13,26 @@ const Demos = () => (
     <SEO title="Demos" />
     <h1>Demo Reels</h1>
     <main>
-      <h3>Character Demo Reel</h3>
-      <AudioPlayer name="Character Demo Reel" volume={0.7} file={demo} />
-      <p>
-        I've done voices that range from large, grungey monsters, to soft-spoken
-        role models, as well as high-pitched, psychotic villians, to focused,
-        battle-torn soldiers.
-      </p>
-      <p>
-        You can find specifics on my characters in the{" "}
-        <Link to="/portfolio">portfolio</Link> section of this website.
-      </p>
-      <br />
-      <AudioPlayer name="Producer Tags Reel" volume={0.7} file={tags} />
-      <p>
-        I both voice and engineer producer tags for use in beats and songs. For
-        a personalized quote, shoot off an email to{" "}
-        <a href="mailto:contact@emnudge.com">Contact@EmNudge.com</a>.
-      </p>
+      <div className="demo-reel">
+        <h3>Character Demo Reel</h3>
+        <AudioPlayer name="Character Demo Reel" volume={0.7} file={demo} />
+        <p>
+          I have done voices that range from large, grungey monsters, to
+          soft-spoken role models, as well as high-pitched, psychotic villians,
+          to focused, battle-torn soldiers. You can find specifics on my
+          characters in the <Link to="/portfolio">portfolio</Link> section of
+          this website.
+        </p>
+      </div>
+      <div className="demo-reel" style={{ "margin-top": "30px" }}>
+        <h3>Producer Tags Demo Reel</h3>
+        <AudioPlayer name="Producer Tags Reel" volume={0.7} file={tags} />
+        <p>
+          I both voice and engineer producer tags for use in beats and songs.
+          For a personalized quote, shoot off an email to{" "}
+          <a href="mailto:contact@emnudge.com">Contact@EmNudge.com</a>.
+        </p>
+      </div>
     </main>
   </Layout>
 )
