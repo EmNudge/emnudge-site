b6af9187cc170e6bed1a5a76ef28876483794c96
calvin.kipperman@gmail.com <emnudge@gmail.com>
2019-11-25 15:10:10 -0500

    changed out table for mobile views

---
+++ src/components/mobileViews/index.js
@@ -0,0 +1,38 @@
+import React from "react"
+import "./index.scss"
+
+const MobileViews = ({ rows, isGame, isAnimation, isNarration }) => {
+  console.log(rows)
+  const getRows = rows => {
+    if (isGame) {
+      return rows.map(({ character, company, title }) => {
+        return { title, role: character, dev: company }
+      })
+    }
+    if (isAnimation) {
+      return rows.map(({ animator, character, title }) => {
+        return { title, role: character, dev: animator }
+      })
+    }
+    if (isNarration) {
+      return rows.map(({ company, title }) => {
+        return { title, dev: company }
+      })
+    }
+    return []
+  }
+
+  return (
+    <div className="mobile-views">
+      {getRows(rows).map(({ title, dev, role }) => (
+        <div key={Math.random()} className="mobile-banner">
+          <h1>{title}</h1>
+          <span>{role}</span>
+          <span>{dev}</span>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default MobileViews
--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -2,7 +2,9 @@ import React from 'react';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
 import Table from '../components/table';
+import MobileViews from '../components/MobileViews';
 import usePortfolio from '../hooks/usePortfolio';
+import './portfolio.scss'
 
 const Portfolio = () => {
 	const { games, narrations, animations } = usePortfolio();
@@ -18,12 +20,15 @@ const Portfolio = () => {
 			<main>
 				<h2>Games</h2>
 				<Table head={['Game', 'Developer', 'Role']} rows={gameRows} />
+				<MobileViews rows={games} isGame />
 				<br />
 				<h2>Animations</h2>
 				<Table head={['Animation', 'Animator', 'Role']} rows={animationRows} />
+				<MobileViews rows={animations} isAnimation />
 				<br />
 				<h2>Narration</h2>
 				<Table head={['Video', 'Company']} rows={narrationRows} />
+				<MobileViews rows={narrations} isNarration />
 			</main>
 		</Layout>
 	);
